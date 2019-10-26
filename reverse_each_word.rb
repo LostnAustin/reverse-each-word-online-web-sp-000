d1802c43572e82afd53a17de691c74d1f1dcd5dd
Chad Lengefeld <chadlengefeld@gmail.com>
2019-10-26 22:03:21 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,6 +4,9 @@
 def reverse_each_word(sentence)
 sentence.split(" ")
 
+sentence.collect do |word|
+  word.reverse
+  sentence.join(" ")
 #.reverse().join("")
   #new = []
   #sentence.collect do |words|
