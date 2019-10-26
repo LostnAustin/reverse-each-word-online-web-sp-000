5008a58976779dc0f6d8a6ee381ef9ac1787e455
Chad Lengefeld <chadlengefeld@gmail.com>
2019-10-26 22:18:24 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,27 +1,16 @@
 
 
 
-def reverse_each_word(sentence)
- new_sent = []
-sentence.split(" ").each do |word|
-   new_sent << word.reverse!
-end
-new_sent.join(" ")
-end
+#  def reverse_each_word(sentence)
+#  new_sent = []
+# sentence.split(" ").each do |word|
+#    new_sent << word.reverse!
+# => end
+#  new_sent.join(" ")
+#  end
 
 
 
 
 
 
-
-#sentence.collect do |word|
-#  word.reverse!
-#{}"#{word}"
-# sentence.join(" ")
-#.reverse().join("")
-  #new = []
-  #sentence.collect do |words|
-  #new << " #{words.reverse!}"
-#end
-#end
