027f3479559c17b6a7c17cdeb6cb8a4129509d87
Chad Lengefeld <chadlengefeld@gmail.com>
2019-10-26 22:10:59 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,15 +2,24 @@
 
 
 def reverse_each_word(sentence)
+  new_sent = []
 sentence.split(" ")
+  sentence.each do |word|
+  new_sent <<  word.reverse
 
-sentence.collect do |word|
-  word.reverse!
-"#{word}"
+
+
+
+
+
+
+#sentence.collect do |word|
+#  word.reverse!
+#{}"#{word}"
 # sentence.join(" ")
 #.reverse().join("")
   #new = []
   #sentence.collect do |words|
   #new << " #{words.reverse!}"
-end
-end
+#end
+#end
